ed2936aefdc61839f82403a15839a75795d0a992
andy <andy_linhao@outlook.com>
2017-09-14 16:19:50 +0800

    lambda表达式对字符串 list进行操作方法demo

--- multimodule/module-lambda/src/main/java/org/andy/LambdaDemo.java
+++ multimodule/module-lambda/src/main/java/org/andy/LambdaDemo.java
@@ -2,7 +2,16 @@ package org.andy;
 
 import org.andy.entity.User;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * @author: andy
@@ -11,7 +20,7 @@ import java.util.Optional;
  */
 public class LambdaDemo {
     public static void main(String[] args){
-        LambdaDemo.test5();
+        LambdaDemo.test13();
     }
 
     /**
@@ -40,7 +49,7 @@ public class LambdaDemo {
     }
     public static void test4(){
         User user=new User();
-        user.setAge(22);
+        user.setAge(10);
         //过滤以后，如果有满足的user（ifPresent）,则打印
         Optional<User> optUser=Optional.ofNullable(user);
         //ifPresent(Consumer<? super T>)当满足条件时执行传入的参数化操作
@@ -54,4 +63,76 @@ public class LambdaDemo {
             return -6L;
         }));
     }
+
+    public static void test6(){
+        String[] strings=new String[]{"abc","def","hhh",""};
+        List<String> lists= Arrays.asList(strings);
+        List<String> tests=new ArrayList<>();
+        lists.forEach(str->{
+            if (null != str && !str.isEmpty()){
+                tests.add(str);
+            }
+        });
+        tests.forEach(System.out::println);
+    }
+    public static void test7(){
+        String[] strings=new String[]{"abc","def","hhh",""};
+        List<String> lists= Arrays.asList(strings);
+        List<String> tests = lists.stream().filter(s -> !s.equals("abc")).collect(Collectors.toList());
+        tests.forEach(System.out::println);
+    }
+    //统计字母以d开头的单词有几个
+    public static  void test8(){
+        String[] strings=new String[]{"abc","def","hhh","dcc"};
+        List<String> lists= Arrays.asList(strings);
+        /*final Function<String,Predicate<String>> startsWithLetter=(String letter)->{
+            Predicate<String> checkStartsWith=(String name)->name.startsWith(letter);
+            return checkStartsWith;
+        };*/
+        //或者
+        final  Function<String,Predicate<String>> startsWithLetter=letter->name->name.startsWith(letter);
+        Long count=lists.stream().filter(startsWithLetter.apply("d")).count();
+        System.out.println(count);
+    }
+    //查询第一个符合某种条件的元素
+    public static void test9(){
+        String[] strings=new String[]{"abc","def","hhh","dcc"};
+        List<String> lists= Arrays.asList(strings);
+        Optional<String> optional=lists.stream().filter(name->name.startsWith("e")).findFirst();
+        System.out.println(optional.orElse("no Name"));
+//        在调用filter后，调用了findFirst方法，这个方法返回的对象类型时Optional。关于这个Optional，
+//        可以将它理解成一个可能存在，也可能不存在的结果。
+//        这样的话，就可以避免对返回结果进行空检查了。对于结果是否真的存在，可以使用isPresent()方法进行判断，
+//        而get()方法用于尝试对结果的获取。当结果不存在时，我们也可以使用orElse()来指定一个替代结果，正如上面使用的那样。
+    }
+    //排序，从小到达排序
+    public static void test10(){
+        Integer[] strings=new Integer[]{10,9,8,99};
+        List<Integer> lists= Arrays.asList(strings);
+   //     lists.sort((a,b)-> Integer.compare(a,b));
+        lists.sort(Comparator.comparing((a)->a.intValue()));
+        lists.forEach(System.out::println);
+    }
+    //查询集合中长度最长的字符串
+    public static void test11(){
+        String[] strings=new String[]{"abc","defe","hhheh","dccccc"};
+        List<String> lists= Arrays.asList(strings);
+        Optional<String> optional=lists.stream().reduce((a,b)->a.length()>=b.length() ? a : b);
+        System.out.println(optional.orElse("no string"));
+    }
+
+    //字符串连接
+    public static void test12(){
+        String[] strings=new String[]{"abc","defe","hhheh","dccccc"};
+        List<String> lists= Arrays.asList(strings);
+        System.out.println(lists.stream().map(String::toUpperCase).collect(Collectors.joining(", ")));
+    }
+    public static void test13(){
+        String str="8bbbc9c";
+       // str.chars().mapToObj(ch->Character.valueOf((char)ch)).forEach(System.out::println);
+        System.out.println("---------------------华丽的分割线");
+        //判断是数字的字符串
+        str.chars().filter(Character::isDigit).forEach(ch->System.out.println((char)ch));
+    }
+
 }
