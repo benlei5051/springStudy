bb3a4679f9453fe8461bcd0efbc8f940f234cba0
andy <andy_linhao@outlook.com>
2017-09-14 18:32:37 +0800

    文件 lambda

--- multimodule/module-lambda/src/main/java/org/andy/LambdaDemo.java
+++ multimodule/module-lambda/src/main/java/org/andy/LambdaDemo.java
@@ -1,17 +1,25 @@
 package org.andy;
 
+import org.andy.entity.Person;
 import org.andy.entity.User;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
 
 /**
  * @author: andy
@@ -19,8 +27,8 @@ import java.util.stream.Collectors;
  * @Description:
  */
 public class LambdaDemo {
-    public static void main(String[] args){
-        LambdaDemo.test13();
+    public static void main(String[] args) throws  Exception{
+        LambdaDemo.test19();
     }
 
     /**
@@ -134,5 +142,84 @@ public class LambdaDemo {
         //判断是数字的字符串
         str.chars().filter(Character::isDigit).forEach(ch->System.out.println((char)ch));
     }
+    //排序
+    public static void test14(){
+        final List<Person> people = Arrays.asList(
+                new Person("John", 20),
+                new Person("Sara", 21),
+                new Person("Jane", 21),
+                new Person("Greg", 35));
+//        List<Person> ascendingAge=people.stream().sorted(Person::ageDifference).collect(Collectors.toList());
+//        ascendingAge.forEach(System.out::println);
+        //降序排列
+        Comparator<Person> compareAscending = (person1, person2) -> person1.ageDifference(person2);
+        Comparator<Person> compareDescending = compareAscending.reversed();
+        people.stream().sorted(compareDescending).collect(Collectors.toList()).forEach(System.out::println);
+    }
+
+    /**
+     * 最大最小值
+     */
+    public static void test15(){
+        final List<Person> people = Arrays.asList(
+                new Person("John", 20),
+                new Person("Sara", 21),
+                new Person("Jane", 21),
+                new Person("Greg", 35));
+        people.stream().min(Person::ageDifference).ifPresent(System.out::println);
+        people.stream().max((person1,person2)->person1.getAge()-person2.getAge()).ifPresent(System.out::println);
+    }
+
+    /**
+     * 按名字排序
+     */
+    public static  void test16(){
+        //将比较的属性抽取出函数接口
+        final Function<Person, String> byName = person -> person.getName();
+        final List<Person> people = Arrays.asList(
+                new Person("John", 20),
+                new Person("Sara", 21),
+                new Person("Jane", 21),
+                new Person("Greg", 35));
+        people.stream().sorted(Comparator.comparing(byName)).collect(Collectors.toList()).forEach(System.out::println);
+    }
+
+    /**
+     * 根据年龄聚合所有的姓名 key 是年龄，value 是姓名集合
+     */
+    public static  void test17(){
+        final List<Person> people = Arrays.asList(
+                new Person("John", 20),
+                new Person("Sara", 21),
+                new Person("Jane", 21),
+                new Person("Greg", 35));
+        //groupingBy方法参数第一个作为分类器，第二个作为对分类结果进行进一步操作的collector。
+        Map<Integer, List<String>> nameOfPeopleByAge = people.stream()
+                .collect(Collectors.groupingBy(Person::getAge, Collectors.mapping(Person::getName, Collectors.toList())));
+        System.out.println(nameOfPeopleByAge);
+
+    }
+    //根据名字的首字母进行分类，分类结果是名字以该首字母起头的年龄最大的人
+    public static void test18(){
+        final List<Person> people = Arrays.asList(
+                new Person("John", 20),
+                new Person("Sara", 21),
+                new Person("Jane", 21),
+                new Person("Greg", 35));
+        Comparator<Person> byAge = Comparator.comparing(Person::getAge);
+        Map<Character, Optional<Person>> oldestPersonInEachAlphabet = people.stream()
+                .collect(Collectors.groupingBy(person -> person.getName().charAt(0), Collectors.reducing(BinaryOperator.maxBy(byAge))));
+        System.out.println("Oldest person in each alphabet: " + oldestPersonInEachAlphabet.get('S'));
+        //以上的groupingBy方法的第二个参数执行了归约(Reduction)操作，而不是之前的映射(Mapping)操作。
+        // 并且利用了BinaryOperator中定义的静态方法maxBy。
+        // 在归约过程中，每次都会取参与的两个元素中较大的那个。最后就得到了整个集合中最大的那个元素。
+    }
+    public static void test19() throws IOException {
+        Files.newDirectoryStream(
+                Paths.get("D:"+ File.separator+"dbeaver"),
+                path -> path.toString().endsWith(".ini"))
+                .forEach(System.out::println);
+    }
+
 
 }
