fbc67deb763e97411be11f32b0aa4bfdf54d7c10
andy <andy_linhao@outlook.com>
2017-11-13 12:44:22 +0800

    test

--- multi-module/module-thread/src/main/java/org/andy/thread/threadPool/ThreadPoolDemo.java
+++ multi-module/module-thread/src/main/java/org/andy/thread/threadPool/ThreadPoolDemo.java
@@ -20,7 +20,6 @@ public class ThreadPoolDemo {
 
     public static void main(String[] args)throws Exception {
      //   testSingleFutureAndCallable();
-        
         testMultFutureAndCallable();
     }
 
