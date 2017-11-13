98ae1583d2a29f36270b9b220fbdb40c905ee709
benlei5051 <andy_linhao@outlook.com>
2017-11-13 11:51:14 +0800

    线程池

--- multi-module/module-thread/src/main/java/org/andy/thread/threadPool/ThreadPoolDemo.java
+++ multi-module/module-thread/src/main/java/org/andy/thread/threadPool/ThreadPoolDemo.java
@@ -20,8 +20,6 @@ public class ThreadPoolDemo {
 
     public static void main(String[] args)throws Exception {
      //   testSingleFutureAndCallable();
-
-
         testMultFutureAndCallable();
     }
 
