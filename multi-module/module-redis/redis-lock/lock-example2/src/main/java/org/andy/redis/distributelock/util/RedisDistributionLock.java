08dfbbc4e27cfbbc732e1cb5653365690af6ce37
andy <andy_linhao@outlook.com>
2018-10-08 18:17:53 +0800

    升级spring boot 2.0.2

--- multi-module/module-redis/redis-lock/lock-example2/src/main/java/org/andy/redis/distributelock/util/RedisDistributionLock.java
+++ multi-module/module-redis/redis-lock/lock-example2/src/main/java/org/andy/redis/distributelock/util/RedisDistributionLock.java
@@ -12,7 +12,7 @@ public interface RedisDistributionLock {
      * @param threadName
      * @return
      */
-    public long lock(String lockKey, String threadName);
+    long lock(String lockKey, String threadName);
 
     /**
      * 解锁， 需要更新加锁时间，判断是否有权限
@@ -20,11 +20,11 @@ public interface RedisDistributionLock {
      * @param lockValue
      * @param threadName
      */
-    public void unlock(String lockKey, long lockValue, String threadName);
+    void unlock(String lockKey, long lockValue, String threadName);
 
     /**
      * 多服务器集群，使用下面的方法，代替System.currentTimeMillis()，获取redis时间，避免多服务的时间不一致问题！！！
      * @return
      */
-    public long currtTimeForRedis();
+    long currtTimeForRedis();
 }
